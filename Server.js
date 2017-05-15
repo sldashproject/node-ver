7457d030884501e3f5dbc9e88fed2345097af829
sldashproject <sldashproject@gmail.com>
2017-05-15 19:23:35 +0000

    adding chart

--- Server.js
+++ Server.js
@@ -1,18 +1,20 @@
-var express = require('express');
+var express = require('express'); //Web server
 var app = express();
-var fs = require('fs');
+var fs = require('fs'); //File system
 
+//listening server
 app.listen(8080, function(){
     console.log("start Server");
 });
 
+//folder setting and read file
 app.get('/', function(req,res){
     fs.readFile('index.html', function(error, data){
        if(error) {
             console.log(error);
        }
        else {
-            res.writeHead(200, {'Content-Type':'text/html'})
+            res.writeHead(200, {'Content-Type':'text/html'});
             res.end(data);
        }
     });
